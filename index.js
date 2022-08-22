a0117a382fef631e2bcaa9467dfcae2aae99b8fc
Gracelaura <gracenjuguna05@gmail.com>
2022-08-22 22:40:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("The Dom has loaded");
+    modifyText()
+});
+
+function modifyText(){
+    const text = document.getElementById("text")
+    text.textContent = "This is really cool!"
+}
